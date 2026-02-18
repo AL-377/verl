8ac4ac7084c3f81f15d5d7838fe09140ccdb4d8f
juntinglu <juntinglu@bytedance.com>
2026-02-18 10:26:49 +0800

    add: riddle preprocess

--- examples/data_preprocess/riddle.py
+++ examples/data_preprocess/riddle.py
@@ -32,6 +32,17 @@ def insert_mc_format_req(raw_prompt:str):
     new_prompt = raw_prompt.replace(old_format_str,format_str)
     return new_prompt
 
+def insert_puzzle_format_req(raw_prompt:str):
+    format_str = """You are an intelligent puzzle solver. You should begin by detailing the internal reasoning process, and then present the answer to the user. The reasoning process should be enclosed within <think> </think> tags, and then output the final option, which should be enclosed within \\boxed{} (e.g. \\boxed{A})
+**OUTPUT FORMAT:**:
+<think> [Your thinking about what answer should be for this riddle] </think>
+\\boxed{Your final option here.}
+---
+"""
+    new_prompt = format_str + raw_prompt
+    return new_prompt
+
+
 def insert_math_format_req(raw_prompt:str):
     old_format_str = "Answer the following math problem and return the answer in the format of \\boxed{answer}"
     format_str = """You are an intelligent math solver. You should begin by detailing the internal reasoning process, and then present the answer to the user. The reasoning process should be enclosed within <think> </think> tags, and then output the final answer, which should be enclosed within \\boxed{}.
@@ -73,6 +84,8 @@ def proprocess_dataset(source_path: str,split: str,save_path: str) -> pd.DataFra
             extra_info["question"] = row["raw_problem"]
             extra_info["answer"] = ground_truth
         elif row["reward_model"]["style"]=="rule-lighteval/MATH_v2":
+            if "AIME" not in row["data_source"]:
+                continue
             data_source = "metaphor_riddle_" + row["data_source"]
             new_prompt_str = insert_math_format_req(row["prompt"][0]["content"])
             prompt = {"role": "user", "content": new_prompt_str}
@@ -85,11 +98,25 @@ def proprocess_dataset(source_path: str,split: str,save_path: str) -> pd.DataFra
             extra_info["question"] = row["raw_problem"]
             extra_info["answer"] = ground_truth
         elif row["reward_model"]["style"]=="rule-boxed_gpqa":
+            if "GPQA" not in row["data_source"]:
+                continue
             data_source = "metaphor_riddle_" + row["data_source"]
             new_prompt_str = insert_mc_format_req(row["prompt"][0]["content"])
             prompt = {"role": "user", "content": new_prompt_str}
             ability = "math"
             ground_truth = row["reward_model"]["ground_truth"]
+            print(ground_truth)
+            reward_model = {"ground_truth": ground_truth, "style": "rule"}
+            extra_info = row["extra_info"]
+            extra_info["split"] = split
+            extra_info["question"] = row["raw_problem"]
+            extra_info["answer"] = ground_truth
+        elif row["reward_model"]["style"]=="rule-logic_puzzle":
+            data_source = "metaphor_riddle_" + row["data_source"]
+            new_prompt_str = insert_puzzle_format_req(row["prompt"][0]["content"])
+            prompt = {"role": "user", "content": new_prompt_str}
+            ability = "math"
+            ground_truth = json.dumps(json.loads(row["reward_model"]["ground_truth"])["answer"])
             print(ground_truth)
             reward_model = {"ground_truth": ground_truth, "style": "rule"}
             extra_info = row["extra_info"]
@@ -111,4 +138,4 @@ def proprocess_dataset(source_path: str,split: str,save_path: str) -> pd.DataFra
 
 if __name__ == "__main__":
     # proprocess_dataset(train_path, "train", "data/train_verl_compat_add_think.parquet")
-    proprocess_dataset(eval_path, "test", "data/eval_verl_compat_gpqa_mathv2.parquet")
+    proprocess_dataset(eval_path, "test", "data/eval_verl_only_gpqa_aime_puzzle.parquet")
